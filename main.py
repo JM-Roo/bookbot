d92608c3436936049ab896d32001aa0cd54d9235
JM-Roo <julianmartinrogers@gmail.com>
2025-08-19 16:28:36 -0700

    Final commit, added sorting of list and path to book.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, char_count
+import sys
+from stats import get_num_words, char_count_dict, sorted_dict_list
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,11 +8,24 @@ def get_book_text(file_path):
 
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
-    character_count = char_count(text)
-    print(character_count)
+    character_count_dict = char_count_dict(text)
+    sorted_dict = sorted_dict_list(character_count_dict)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in range(len(sorted_dict)):
+        print(f"{sorted_dict[i]["char"]}: {sorted_dict[i]["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ def get_num_words(text):
     return num_words
 
 
-def char_count(text):
+def char_count_dict(text):
     count_dict = {}
     lower_text = text.lower()
 
@@ -15,3 +15,18 @@ def char_count(text):
             count_dict[str(character)] =1
     
     return count_dict
+
+def sort_on(item):
+    return item["num"]
+
+def sorted_dict_list(input_dict):
+    output_list = []
+    for character in input_dict:
+        if character.isalpha() == True:
+            temp_dict = {}
+            temp_dict["char"] = character
+            temp_dict["num"] = input_dict[character]
+            output_list.append(temp_dict)
+        
+    output_list.sort(reverse=True, key=sort_on)
+    return output_list
